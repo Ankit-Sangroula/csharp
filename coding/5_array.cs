1378acbe01e63b903409ced5b8adefcf212ec2b9
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-02 08:23:08 +0545

    Add array sum program using foreach and for loop

---
+++ coding/5_array.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Array
+{
+    static void Main()
+    {
+        Console.Write("How many numbers do you want to enter? ");
+        int count = int.Parse(Console.ReadLine());
+        int[] a = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("Enter number " + (i + 1) + ": ");
+            a[i] = int.Parse(Console.ReadLine());
+        }
+
+        int sum = 0;
+
+        foreach (int number in a)
+        {
+            sum = sum + number;
+        }
+
+        Console.WriteLine("Sum = " + sum);
+    }
+}
+
+
+
+
+//without foreach
+//using System;
+
+//class Array
+//{
+//    static void Main()
+//    {
+//        Console.Write("How many numbers do you want to enter? ");
+//        int count = int.Parse(Console.ReadLine());
+
+//        int[] a = new int[count]; 
+
+//        for (int i = 0; i < count; i++)
+//        {
+//            Console.Write("Enter number " + (i + 1) + ": ");
+//            a[i] = int.Parse(Console.ReadLine());
+//        }
+
+//        int sum = 0;
+
+//        for (int i = 0; i < a.Length; i++) 
+//        {
+//            sum = sum + a[i]; 
+//        }
+
+//        Console.WriteLine("Sum = " + sum);
+//    }
+//}
