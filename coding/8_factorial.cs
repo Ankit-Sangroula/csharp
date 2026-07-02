360cfb7fadaa3f3567d97cfdb844c710373a94d3
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-02 08:24:11 +0545

    Add factorial calculator program in C#

---
+++ coding/8_factorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Factorial
+{
+    static void Main()
+    {
+        Console.Write("Enter a Number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int fact = 1;
+        for(int i = 1; i <= n; i++) {
+         fact = fact * i;
+        }
+        Console.WriteLine("Factorial of " + n + " is: " + fact);
+    }
+
+}
