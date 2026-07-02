78b190632f2a7a1146ed4dd9a63020c792b25833
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-02 08:23:30 +0545

    Add C# program to check if a number is a palindrome

---
+++ coding/6_palindrome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Palindrome
+{
+    static void Main()
+    {
+        Console.Write("Enter a Number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int original = n;
+        int reverse = 0;
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        if (original == reverse)
+        {
+            Console.WriteLine(original + " is a Palindrome");
+        }
+        else
+        {
+            Console.WriteLine(original + " is not a Palindrome");
+        }
+    }
+}
