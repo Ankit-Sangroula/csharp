7128e25ea7fca85c1256969fd6b2cd4dced97412
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-03 20:06:53 +0545

    Comment out all code in typeconversion.cs
    All code in typeconversion.cs has been commented out, disabling its execution and leaving the file with no active functionality.

--- coding/Basics/typeconversion.cs
+++ coding/Basics/typeconversion.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿//using System;
 
-class TypeConversion
-{
-    static void Main()
-    {
-        double salary = 50000.95
-        int round
-        int roundown = (int)price;
-        Console.WriteLine("Original:" + price);
-        Console.WriteLine("Cast to int:" + roundown);
-    }
+//class TypeConversion
+//{
+//    static void Main()
+//    {
+//        double salary = 50000.95
+//        int round
+//        int roundown = (int)price;
+//        Console.WriteLine("Original:" + price);
+//        Console.WriteLine("Cast to int:" + roundown);
+//    }
 
-}
+//}
