74c680c70a6f360e5e18608d84c6d0d868d18f01
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-06 21:57:06 +0545

    Add ArrayExample class to compute and print average score
    Introduced ArrayExample class with a Main method that initializes an array of scores, computes their sum and average, and outputs the average to the console.

---
+++ coding/Basics/array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ArrayExample
+{
+    static void Main()
+    {
+        int[] scores = { 85, 92, 78, 60, 99 };
+        int sum = 0;
+
+        foreach (int score in scores)
+        {
+            sum += score;
+        }
+
+        double average = (double)sum / scores.Length;
+        Console.WriteLine("Average: " + average);
+    }
+}
