fd274d2f6a163b709cf728502645443ac5ff82b0
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-06 21:57:30 +0545

    Add Car class with constructor and Drive method example
    Introduced a Car class in C# with a constructor to set color and speed properties. Added a Drive method to display car details and a Main method to demonstrate object creation and usage.

---
+++ coding/Basics/constructor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Car
+{
+    public string color;
+    public int speed;
+
+    public Car(string carColor, int carSpeed)
+    {
+        color = carColor;
+        speed = carSpeed;
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine(color + " car driving at " + speed + " km/h");
+    }
+}
+
+class ConstructorExample
+{
+    static void Main()
+    {
+        Car myCar = new Car("Black", 150); // values set immediately at creation
+        myCar.Drive();
+    }
+}
