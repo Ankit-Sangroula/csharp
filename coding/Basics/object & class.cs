dac325d0f14aba3d1109070624bba12d1247aa3b
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-06 21:57:52 +0545

    Add Car class and demo object usage in Main method
    Introduced a Car class with color and speed properties and a Drive() method. Added a Vechicle class with Main() to create and demonstrate multiple Car objects.

---
+++ coding/Basics/object & class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Car
+{
+    public string color;
+    public int speed;
+
+    public void Drive()
+    {
+        Console.WriteLine(color + " car is driving at " + speed + " km/h");
+    }
+}
+
+class Vechicle
+{
+    static void Main()
+    {
+        Car myCar = new Car();     // create an object from the Car blueprint
+        myCar.color = "Red";
+        myCar.speed = 120;
+        myCar.Drive();
+
+        Car anotherCar = new Car(); // a separate, independent object
+        anotherCar.color = "Blue";
+        anotherCar.speed = 80;
+        anotherCar.Drive();
+    }
+}
