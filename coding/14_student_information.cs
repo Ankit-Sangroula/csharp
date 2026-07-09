e6abd743cc18e0340e1f3259abf98ff9dc184694
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-09 07:49:24 +0545

    Add StudentInformation and StudentDetails classes
    Implemented StudentInformation class with properties, constructor, and display method. Added StudentDetails class with Main method to demonstrate usage.

---
+++ coding/14_student_information.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class StudentInformation  
+{
+    public int RollNo { get; set; }
+    public string Name { get; set; }
+    public string Faculty { get; set; }
+
+    public StudentInformation(int rollNo, string name, string faculty)   
+    {
+        RollNo = rollNo;
+        Name = name;
+        Faculty = faculty;
+    }
+
+    // Method
+    public void Display()   
+    {
+        Console.WriteLine($"RollNo: {RollNo}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Faculty: {Faculty}");
+    }
+}
+
+class StudentDetails   
+{
+    static void Main(string[] args)
+    {
+        StudentInformation student1 = new StudentInformation(1, "Pranish", "Bsc.CSIT");
+        StudentInformation student2 = new StudentInformation(2, "Bipin", "BBS");
+
+        student1.Display();
+        student2.Display();
+    }
+}
