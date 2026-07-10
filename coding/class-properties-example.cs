932739b62d45f972dee5870bf5a5ec6eae51cc34
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-10 06:08:35 +0545

    Add commented-out EmployeeExample class with sample code
    Added a commented C# EmployeeExample class with properties, constructor, and methods for employee data management. Also included a nested EmployeeShow class with an incomplete Main method. The code is non-functional, contains syntax errors, and serves as a draft or placeholder.

---
+++ coding/class-properties-example.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+
+//class EmployeeExample
+//{
+//    private firstName;
+//    private lastName;
+//    private salary;
+//        public string FirstName
+//    {
+//        get { return firstName; }
+//        set { FirstName = value; }
+//    }
+
+//    public string LastName
+//    {
+//        get { return lastName; }
+//        set { LastName = value; }
+//    }
+
+//    public double Salary
+//    {
+//        get { return salary; }
+//        set { Salary = value; }
+//    }
+
+//    public EmployeeExample(string firstName, string lastName, double salary)
+//    {
+//        this.FirstName = firstName;
+//        this.LastName = lastName;
+//        this.Salary = salary;
+//    }
+//    public void ShowFullName()
+//    {
+//        Console.WriteLine("Full Name:" + FirstName + " " + LastName);
+//    }
+//    public void IncrementSalary(double s)
+//    {
+//        Salary += s;
+//    }
+
+//    class EmployeeShow
+//    {
+//        static void Main(string[] args)
+//        {
+//            EmployeeShow emp = new Employee("Ram", "Bahadur", 20000)
+//                emp.
+//                }
+//    }
