2ab790c605b75515e5c4af8766ce0408797d1aca
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-15 22:02:59 +0545

    Add LINQ example with simple program

---
+++ coding/19_LINQ_example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class linqExample
+{
+
+    //using Without LINQ
+    //static void Main()
+    //{
+    //    List<int> marks = new() { 45, 80, 65,90,35,75};
+    //    List<int> result = new List<int>();
+    //    foreach (int mark in marks)
+    //    {
+    //        if (mark > 60)
+    //        {
+    //            result.Add(mark);
+    //        }
+    //    }
+    //        foreach(int mark in result) 
+    //    {
+    //            Console.WriteLine(mark);
+    //        }
+
+    //}
+
+
+    //with LINQ
+    static void Main()
+    {
+        List<int> marks = new() { 45, 80, 65,90,35,75};
+       var result = marks.Where(m => m > 60);
+        foreach (var mark in result) 
+        {
+                Console.WriteLine(mark);
+            }
+        
+    }
+}
