9703e19eeb3d5b0bff9ba11b0638611a92649faa
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-15 22:03:56 +0545

    Add example to compute sum and average of integer list
    Added 22_file_example3.cs, a C# program that demonstrates creating a list of integers, calculating their sum and average using LINQ methods, and printing the results to the console.

---
+++ coding/22_file_example3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class fileExample3
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average: " + average);
+    }
+}
