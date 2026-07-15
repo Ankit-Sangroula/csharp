9da2d26d9e7bb60438d2bf36a701fb713979da97
Ankit.3k <ankitsangroula2006@gmail.com>
2026-07-15 22:03:32 +0545

    Add fileExample2: squares list and prints results
    Added a new C# program in fileExample2 that initializes a list of integers, computes their squares using LINQ's Select, and prints the squared values to the console. Included the required using directive.

---
+++ coding/21_file_example2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class fileExample2
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        List<int> squared = numbers.Select(n => n * n).ToList();
+
+        Console.WriteLine("Squared list:");
+        foreach (int num in squared)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
